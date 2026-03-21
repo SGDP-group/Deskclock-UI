3cb712cd8f72f6a9832b728d3756628c5038d8ef
unknown <cliyanage124@gmail.com>
2026-03-22 03:16:52 +0530

    Fix Antonio font initializer for LVGL v9

--- src/fonts/Antonio_bold_80.c
+++ src/fonts/Antonio_bold_80.c
@@ -1437,7 +1437,9 @@ lv_font_t Antonio_bold_80 = {
     .underline_position = 0,
     .underline_thickness = 0,
 #endif
+#if LVGL_VERSION_MAJOR < 9
     .static_bitmap = 0,
+#endif
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
     .fallback = &lv_font_montserrat_48,
